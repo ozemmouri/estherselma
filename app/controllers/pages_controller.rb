4d910462cac5d7fc915cb30e055e2fe272bf4ee0
Ghali Dhobb <ghalidhobb@gmail.com>
2023-09-22 15:11:12 +0100

    taboun mhom

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,28 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def festival
+  end
+
+  def edition2023
+  end
+
+  def paroles
+  end
+
+  def tissuculturel
+  end
+
+  def programme
+  end
+
+  def infopratiques
+  end
+
+  def billeterie
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "pages#home"
+  get 'festival', to: "pages#festival"
+  get 'edition2023', to: "pages#edition2023"
+  get 'paroles', to: "pages#paroles"
+  get 'tissu-culturel', to: "pages#tissuculturel"
+  get 'programme', to: "pages#programme"
+  get 'infos-pratiques', to: "pages#infopratiques"
+  get 'billeterie', to: "pages#billeterie"
+  get 'contact', to: "pages#contact"
 end
